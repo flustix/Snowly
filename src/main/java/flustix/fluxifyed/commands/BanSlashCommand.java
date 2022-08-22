705705e72f74f7f59f3a8da3863bcb95e396a8dd
Flux <flustixshinu@gmail.com>
2022-08-22 17:58:16 +0200

    redesign ban command (also fix a security issue LMAO)

--- src/main/java/flustix/fluxifyed/commands/BanSlashCommand.java
+++ src/main/java/flustix/fluxifyed/commands/BanSlashCommand.java
@@ -1,7 +1,10 @@
 package flustix.fluxifyed.commands;
 
+import flustix.fluxifyed.Main;
 import flustix.fluxifyed.command.SlashCommand;
+import flustix.fluxifyed.utils.permissions.PermissionLevel;
 import flustix.fluxifyed.utils.slash.SlashCommandUtils;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
@@ -9,29 +12,44 @@ import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 public class BanSlashCommand extends SlashCommand {
     public BanSlashCommand() {
         super("ban", "Bans people");
-        this.addOption(OptionType.USER, "target", "The user to ban", true, false);
-        this.addOption(OptionType.STRING, "reason", "The reason for the ban", false, false);
+        setPermissionLevel(PermissionLevel.MODERATOR);
+        addOption(OptionType.USER, "target", "The user to ban", true, false);
+        addOption(OptionType.STRING, "reason", "The reason for the ban", false, false);
     }
 
     public void execute(SlashCommandInteraction interaction) {
         OptionMapping target = interaction.getOption("target");
         OptionMapping reason = interaction.getOption("reason");
 
-        if (target == null) {
-            SlashCommandUtils.reply(interaction, "how the fuck did you even get here");
-            return;
-        }
-
         String reasonText = reason == null ? "No reason" : reason.getAsString();
 
         try {
             interaction.getGuild().ban(target.getAsUser(), 7, reasonText).queue((v) -> {
-                interaction.reply(":white_check_mark: Banned " + target.getAsUser().getAsTag() + " for " + reasonText).queue();
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setTitle(":white_check_mark: Banned user!")
+                        .addField(":bust_in_silhouette: User", target.getAsUser().getAsTag(), true)
+                        .addField(":hammer: Banned by", interaction.getUser().getAsTag(), true)
+                        .addField(":scroll: Reason", reasonText, false)
+                        .setColor(Main.accentColor);
+
+                interaction.replyEmbeds(embed.build()).queue();
             }, (error) -> {
-                interaction.reply(":x: I can't ban that user: " + error.getMessage()).queue();
+                EmbedBuilder embed = new EmbedBuilder()
+                        .setTitle(":x: Failed to ban user!")
+                        .addField(":bust_in_silhouette: User", target.getAsUser().getAsTag(), true)
+                        .addField(":x: Error", error.getMessage(), false)
+                        .setColor(Main.accentColor);
+
+                interaction.replyEmbeds(embed.build()).queue();
             });
         } catch (Exception e) {
-            SlashCommandUtils.reply(interaction, ":x: Failed to ban user: " + e.getMessage());
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle(":x: Failed to ban user!")
+                    .addField(":bust_in_silhouette: User", target.getAsUser().getAsTag(), true)
+                    .addField(":x: Error", e.getMessage(), false)
+                    .setColor(Main.accentColor);
+
+            SlashCommandUtils.reply(interaction, embed.build());
         }
     }
 }
