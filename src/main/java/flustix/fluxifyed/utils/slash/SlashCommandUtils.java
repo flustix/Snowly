9e635a2911aff3b26a2462e077214af38ea9e1db
Flux <flustixshinu@gmail.com>
2022-09-03 11:33:07 +0200

    add some more stuff to SlashCommandUtils

--- src/main/java/flustix/fluxifyed/utils/slash/SlashCommandUtils.java
+++ src/main/java/flustix/fluxifyed/utils/slash/SlashCommandUtils.java
@@ -1,5 +1,6 @@
 package flustix.fluxifyed.utils.slash;
 
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.interactions.InteractionHook;
@@ -9,20 +10,19 @@ import java.util.function.Consumer;
 
 public class SlashCommandUtils {
     public static void reply(SlashCommandInteraction interaction, String content) {
-        interaction.reply(content).complete();
+        reply(interaction, new MessageBuilder().setContent(content).build());
     }
 
     public static void reply(SlashCommandInteraction interaction, Message message) {
-        interaction.reply(message).complete();
+        reply(interaction, message, (hook)-> {});
     }
 
     public static void reply(SlashCommandInteraction interaction, MessageEmbed embed) {
-        interaction.replyEmbeds(embed).complete();
+        reply(interaction, new MessageBuilder().setEmbeds(embed).build());
     }
 
     public static void reply(SlashCommandInteraction interaction, String content, Consumer<InteractionHook> callback) {
-        InteractionHook hook = interaction.reply(content).complete();
-        callback.accept(hook);
+        reply(interaction, new MessageBuilder().setContent(content).build(), callback);
     }
 
     public static void reply(SlashCommandInteraction interaction, Message message, Consumer<InteractionHook> callback) {
@@ -31,7 +31,31 @@ public class SlashCommandUtils {
     }
 
     public static void reply(SlashCommandInteraction interaction, MessageEmbed embed, Consumer<InteractionHook> callback) {
-        InteractionHook hook = interaction.replyEmbeds(embed).complete();
+        reply(interaction, new MessageBuilder().setEmbeds(embed).build(), callback);
+    }
+
+    public static void replyEphemeral(SlashCommandInteraction interaction, String content) {
+        replyEphemeral(interaction, new MessageBuilder().setContent(content).build());
+    }
+
+    public static void replyEphemeral(SlashCommandInteraction interaction, MessageEmbed embed) {
+        replyEphemeral(interaction, new MessageBuilder().setEmbeds(embed).build());
+    }
+
+    public static void replyEphemeral(SlashCommandInteraction interaction, Message message) {
+        replyEphemeral(interaction, message, (hook)-> {});
+    }
+
+    public static void replyEphemeral(SlashCommandInteraction interaction, String content, Consumer<InteractionHook> callback) {
+        replyEphemeral(interaction, new MessageBuilder().setContent(content).build(), callback);
+    }
+
+    public static void replyEphemeral(SlashCommandInteraction interaction, MessageEmbed embed, Consumer<InteractionHook> callback) {
+        replyEphemeral(interaction, new MessageBuilder().setEmbeds(embed).build(), callback);
+    }
+
+    public static void replyEphemeral(SlashCommandInteraction interaction, Message message, Consumer<InteractionHook> callback) {
+        InteractionHook hook = interaction.reply(message).setEphemeral(true).complete();
         callback.accept(hook);
     }
 }
