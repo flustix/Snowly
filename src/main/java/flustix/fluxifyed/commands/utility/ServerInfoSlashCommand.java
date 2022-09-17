1816416a86a280688793688366078d790aa291ca
Flux <flustixshinu@gmail.com>
2022-09-17 23:33:19 +0200

    change some stuff in ServerInfoSlashCommand

--- src/main/java/flustix/fluxifyed/commands/utility/ServerInfoSlashCommand.java
+++ src/main/java/flustix/fluxifyed/commands/utility/ServerInfoSlashCommand.java
@@ -4,11 +4,9 @@ import flustix.fluxifyed.Main;
 import flustix.fluxifyed.command.SlashCommand;
 import flustix.fluxifyed.utils.slash.SlashCommandUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.temporal.ChronoField;
 
 public class ServerInfoSlashCommand extends SlashCommand {
     public ServerInfoSlashCommand() {
@@ -24,19 +22,14 @@ public class ServerInfoSlashCommand extends SlashCommand {
         embed.addField(":1234: Server ID", interaction.getGuild().getId(), true);
         embed.addField(":crown: Server Owner", interaction.getGuild().getOwner().getAsMention(), true);
         embed.addField(":busts_in_silhouette: Member Count", interaction.getGuild().getMemberCount() + "", true);
-        embed.addField(":clock1: Server Creation Date", interaction.getGuild().getTimeCreated().toString(), true);
+        embed.addField(":clock1: Server Creation Date", "<t:" + interaction.getGuild().getTimeCreated().getLong(ChronoField.INSTANT_SECONDS) + ":f>", true);
         embed.addField(":books: Channels",
                 "Total: " + interaction.getGuild().getChannels().size() + "\n"
                 + ":pencil: Text Channels: " + interaction.getGuild().getTextChannels().size() + "\n"
                 + ":sound: Voice Channels: " + interaction.getGuild().getVoiceChannels().size(),
                 true);
         embed.addField(":moyai: Emojis", interaction.getGuild().getEmojis().size() + "", true);
-
-        List<String> roles = new ArrayList<>();
-        for (Role role : interaction.getGuild().getRoles()) {
-            roles.add(role.getAsMention());
-        }
-        embed.addField(":scroll: Roles", String.join(", ", roles), false);
+        embed.addField(":scroll: Roles", interaction.getGuild().getRoles().size() + "", true);
 
         SlashCommandUtils.reply(interaction, embed.build());
     }
