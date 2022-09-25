76b7b5f9d6474f6c680f388a14b39a76c5a56de6
Flux <flustixshinu@gmail.com>
2022-09-25 15:39:13 +0200

    add modules to guild component

--- src/main/java/flustix/fluxifyed/database/api/components/APIGuild.java
+++ src/main/java/flustix/fluxifyed/database/api/components/APIGuild.java
@@ -1,5 +1,7 @@
 package flustix.fluxifyed.database.api.components;
 
+import flustix.fluxifyed.settings.GuildSettings;
+import flustix.fluxifyed.settings.Settings;
 import net.dv8tion.jda.api.entities.Guild;
 
 public class APIGuild {
@@ -9,11 +11,18 @@ public class APIGuild {
     public String icon;
     public String banner;
 
+    public boolean xpEnabled;
+    public boolean shopEnabled;
+
     public APIGuild(Guild guild) {
         id = guild.getId();
         name = guild.getName();
         owner = guild.getOwner().getUser().getId();
         icon = guild.getIconUrl();
         banner = guild.getBannerUrl();
+
+        GuildSettings settings = Settings.getGuildSettings(guild.getId());
+        xpEnabled = settings.xpEnabled();
+        shopEnabled = settings.shopEnabled();
     }
 }
