69b1cd043c48f62f3615f2bc858e0604c6a68ba6
Flux <flustixshinu@gmail.com>
2022-10-04 16:29:43 +0200

    warnings...

--- src/main/java/flustix/fluxifyed/utils/file/FileUtils.java
+++ src/main/java/flustix/fluxifyed/utils/file/FileUtils.java
@@ -1,12 +1,17 @@
 package flustix.fluxifyed.utils.file;
 
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class FileUtils {
     public static String getResourceString(String path) {
         try {
-            return new Scanner(FileUtils.class.getResourceAsStream(path), StandardCharsets.UTF_8).useDelimiter("\\A").next();
+            InputStream stream = FileUtils.class.getResourceAsStream(path);
+            if (stream == null) return "";
+
+            Scanner scanner = new Scanner(stream, StandardCharsets.UTF_8);
+            return scanner.useDelimiter("\\A").next();
         } catch (Exception e) {
             return "";
         }
