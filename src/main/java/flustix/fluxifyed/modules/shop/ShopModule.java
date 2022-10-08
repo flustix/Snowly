89d6be43b568ddf215f24f97692cde7d2d4d34d6
Flux <flustixshinu@gmail.com>
2022-10-08 13:06:58 +0200

    add the other wip modules

---
+++ src/main/java/flustix/fluxifyed/modules/shop/ShopModule.java
@@ -0,0 +1,10 @@
+package flustix.fluxifyed.modules.shop;
+
+import flustix.fluxifyed.components.Module;
+
+public class ShopModule extends Module {
+    public ShopModule() {
+        super("shop", "Shop", "Create a shop for your members to buy roles and other items.");
+    }
+}
+
---
+++ src/main/java/flustix/fluxifyed/modules/welcome/WelcomeModule.java
@@ -0,0 +1,9 @@
+package flustix.fluxifyed.modules.welcome;
+
+import flustix.fluxifyed.components.Module;
+
+public class WelcomeModule extends Module {
+    public WelcomeModule() {
+        super("welcome", "Welcome", "Customizable welcome messages for new members.");
+    }
+}
