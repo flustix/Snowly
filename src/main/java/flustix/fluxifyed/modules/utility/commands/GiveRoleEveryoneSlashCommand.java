7d7456b5066a70b8a4861589c5a69ff17aee1494
Flux <flustixshinu@gmail.com>
2022-10-11 17:23:38 +0200

    attempt #4 (actually reading the docs)

--- src/main/java/flustix/fluxifyed/modules/utility/commands/GiveRoleEveryoneSlashCommand.java
+++ src/main/java/flustix/fluxifyed/modules/utility/commands/GiveRoleEveryoneSlashCommand.java
@@ -7,7 +7,7 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This is absolute overkill. I expect this to kill the bot.
@@ -20,21 +20,23 @@ public class GiveRoleEveryoneSlashCommand extends SlashCommand {
     }
 
     public void execute(SlashCommandInteraction interaction) {
-        int count = 0;
+        AtomicInteger count = new AtomicInteger();
 
         interaction.reply("Starting...").complete();
 
         try {
-            interaction.getGuild().loadMembers().get();
-            List<Member> members = interaction.getGuild().getMembers();
-
-            for (Member member : members) {
-                if (!member.getRoles().contains(interaction.getOption("role").getAsRole())) {
-                    interaction.getGuild().addRoleToMember(member, interaction.getOption("role").getAsRole()).queue();
-                }
-                count++;
-            }
-            interaction.getChannel().sendMessage("Giving " + count + " members the role.").complete();
+            interaction.getGuild()
+                    .retrieveMembers(new ArrayList<>())
+                    .onSuccess(members -> {
+                        for (Member member : members) {
+                            if (!member.getRoles().contains(interaction.getOption("role").getAsRole())) {
+                                interaction.getGuild().addRoleToMember(member, interaction.getOption("role").getAsRole()).queue();
+                            }
+                            count.getAndIncrement();
+                        }
+                        interaction.getChannel().sendMessage("Giving " + count + " members the role.").complete();
+                    }
+            );
         } catch (Exception e) {
             e.printStackTrace();
             interaction.getChannel().sendMessage("Something went wrong: " + e.getMessage()).complete();
