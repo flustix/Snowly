94a6b94b30583aa43406b7538748b2c68f2ec5f7
Flux <flustixshinu@gmail.com>
2022-10-23 12:29:25 +0200

    support command

---
+++ src/main/java/flustix/fluxifyed/modules/utility/commands/SupportSlashCommand.java
@@ -0,0 +1,23 @@
+package flustix.fluxifyed.modules.utility.commands;
+
+import flustix.fluxifyed.Main;
+import flustix.fluxifyed.components.SlashCommand;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+
+public class SupportSlashCommand extends SlashCommand {
+    public SupportSlashCommand() {
+        super("support", "Gives you the link to the support server.");
+    }
+
+    @Override
+    public void execute(SlashCommandInteraction interaction) {
+        MessageCreateBuilder message = new MessageCreateBuilder()
+                .setContent("After you join the support server, ask your question in the #fluxifyed-support forum channel.")
+                .addActionRow(Button.link("https://discord.gg/GaKKeWg", "Join the support server."));
+
+        interaction.reply(message.build()).queue();
+    }
+}
