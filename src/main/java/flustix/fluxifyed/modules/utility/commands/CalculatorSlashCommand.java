232734d53400595941e8a3810304e277e2fec2d8
Flux <flustixshinu@gmail.com>
2022-10-23 23:07:15 +0200

    make the calc command an embed (and remove rooot's stupid stuff)

--- src/main/java/flustix/fluxifyed/modules/utility/commands/CalculatorSlashCommand.java
+++ src/main/java/flustix/fluxifyed/modules/utility/commands/CalculatorSlashCommand.java
@@ -1,6 +1,8 @@
 package flustix.fluxifyed.modules.utility.commands;
 
+import flustix.fluxifyed.Main;
 import flustix.fluxifyed.components.SlashCommand;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
@@ -18,20 +20,26 @@ public class CalculatorSlashCommand extends SlashCommand {
         if (equationMapping == null) return;
 
         try {
-            Expression expression = new ExpressionBuilder(equationMapping.getAsString()).variable("amongus").build();
-            expression.setVariable("amongus", 69);
+            Expression expression = new ExpressionBuilder(equationMapping.getAsString()).build();
+            String resultString = String.valueOf(expression.evaluate());
 
-            double result = expression.evaluate();
-
-            String resultString = String.valueOf(result);
-
-            if (resultString.endsWith(".0")) {
+            if (resultString.endsWith(".0"))
                 resultString = resultString.substring(0, resultString.length() - 2);
-            }
 
-            interaction.reply(resultString).queue();
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle("Calculator")
+                    .addField(":asterisk: Equation", equationMapping.getAsString(), false)
+                    .addField(":1234: Result", resultString, false)
+                    .setColor(Main.accentColor);
+
+            interaction.replyEmbeds(embed.build()).queue();
         } catch (Exception e) {
-            interaction.reply("Invalid equation!").queue();
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setTitle("Calculator")
+                    .addField(":x: Error", "Invalid equation", false)
+                    .setColor(0xFF5555);
+
+            interaction.replyEmbeds(embed.build()).queue();
         }
     }
 }
