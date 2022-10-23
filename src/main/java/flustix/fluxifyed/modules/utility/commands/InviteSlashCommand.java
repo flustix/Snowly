de75fe534569c0190f2926453c8b87981bb7fd01
Flux <flustixshinu@gmail.com>
2022-10-23 12:25:53 +0200

    invite command

---
+++ src/main/java/flustix/fluxifyed/modules/utility/commands/InviteSlashCommand.java
@@ -0,0 +1,22 @@
+package flustix.fluxifyed.modules.utility.commands;
+
+import flustix.fluxifyed.Main;
+import flustix.fluxifyed.components.SlashCommand;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
+
+public class InviteSlashCommand extends SlashCommand {
+    public InviteSlashCommand() {
+        super("invite", "Get the invite link for the bot");
+    }
+
+    public void execute(SlashCommandInteraction interaction) {
+        MessageCreateBuilder message = new MessageCreateBuilder()
+                .setContent("Here you go!")
+                .addActionRow(Button.link(Main.getBot().getInviteUrl(Permission.ADMINISTRATOR), "Invite to guild..."));
+
+        interaction.reply(message.build()).queue();
+    }
+}
