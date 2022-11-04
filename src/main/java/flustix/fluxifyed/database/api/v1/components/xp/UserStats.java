61b996e3f1a8fb43166add188b40a2c969292494
Flux <flustixshinu@gmail.com>
2022-11-04 19:39:04 +0100

    return username, discriminator and avatar in `UserStats`

--- src/main/java/flustix/fluxifyed/database/api/v1/components/xp/UserStats.java
+++ src/main/java/flustix/fluxifyed/database/api/v1/components/xp/UserStats.java
@@ -6,12 +6,16 @@ import flustix.fluxifyed.modules.xp.components.XPGuild;
 import flustix.fluxifyed.modules.xp.components.XPUser;
 import flustix.fluxifyed.utils.xp.XPUtils;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserStats {
+    public String username;
+    public String discriminator;
+    public String avatar;
     public String id;
     public int level;
     public int xp;
@@ -25,6 +29,23 @@ public class UserStats {
         this.xp = xuser.getXP();
         this.rank = xguild.getTop().indexOf(xuser) + 1;
         this.guild = new UserStatsGuild(Main.getBot().getGuildById(xguild.getID()));
+
+        User user = Main.getBot().getUserById(xuser.getID());
+        if (user == null) {
+            try { // user was not cached, fetch from discord
+                user = Main.getBot().retrieveUserById(xuser.getID()).complete();
+            } catch (Exception ignored) {}
+        }
+
+        if (user != null) {
+            this.username = user.getName();
+            this.discriminator = user.getDiscriminator();
+            this.avatar = user.getAvatarUrl();
+        } else { // user still wasn't found, use placeholder
+            this.username = "Unknown";
+            this.discriminator = "0000";
+            this.avatar = "https://cdn.discordapp.com/embed/avatars/0.png";
+        }
 
         chartEntries = new ArrayList<>();
 
