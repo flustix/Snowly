65e8dbf2c80d07eafac2b4249842b97cc288525d
Flux <flustixshinu@gmail.com>
2022-11-12 18:31:02 +0100

    i hate cors i hate cors i hate cors

--- src/main/java/flustix/fluxifyed/database/api/v1/Router.java
+++ src/main/java/flustix/fluxifyed/database/api/v1/Router.java
@@ -93,6 +93,8 @@ public class Router implements HttpHandler {
 
         headers.set("Content-Type", String.format("application/json; charset=%s", StandardCharsets.UTF_8));
         headers.set("Access-Control-Allow-Origin", "*");
+        headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
         final byte[] rawResponse = json.toString().getBytes(StandardCharsets.UTF_8);
         exchange.sendResponseHeaders(200, rawResponse.length);
         exchange.getResponseBody().write(rawResponse);
