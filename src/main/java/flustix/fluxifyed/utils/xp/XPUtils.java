c6bef874f7c4844b59b69090e530e9322e52c30c
Flux <flustixshinu@gmail.com>
2022-12-20 23:16:29 +0100

    add different leveling system for easier adapting

--- src/main/java/flustix/fluxifyed/utils/xp/XPUtils.java
+++ src/main/java/flustix/fluxifyed/utils/xp/XPUtils.java
@@ -2,10 +2,38 @@ package flustix.fluxifyed.utils.xp;
 
 public class XPUtils {
     public static int calculateLevel(int xp) {
-        return (int) Math.floor(Math.sqrt(xp / 100f));
+        return calculateLevel(xp, "default");
+    }
+
+    /**
+     * Calculates level from xp with different formulas from other bots (for easier adapting)
+     */
+    public static int calculateLevel(int xp, String formula) {
+        switch (formula) {
+            case "amari" -> {
+                return (int) Math.sqrt(xp / 20f - 35) + 1;
+            }
+            default -> {
+                return (int) Math.floor(Math.sqrt(xp / 100f));
+            }
+        }
     }
 
     public static int calculateXP(int level) {
-        return 100 * (level * level);
+        return calculateXP(level, "default");
+    }
+
+    /**
+     * Calculates xp from level with different formulas from other bots (for easier adapting)
+     */
+    public static int calculateXP(int level, String formula) {
+        switch (formula) {
+            case "amari" -> {
+                return (int) (20 * Math.pow(level - 1, 2) + 35);
+            }
+            default -> {
+                return 100 * (level * level);
+            }
+        }
     }
 }
