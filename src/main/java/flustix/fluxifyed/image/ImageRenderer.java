2c90f93fd20e3a213ae2fb92fc72ef6f35e58c53
Flux <flustixshinu@gmail.com>
2023-03-07 22:25:38 +0100

    rewrite image rendering

--- src/main/java/flustix/fluxifyed/image/ImageRenderer.java
+++ src/main/java/flustix/fluxifyed/image/ImageRenderer.java
@@ -1,8 +1,5 @@
 package flustix.fluxifyed.image;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import flustix.fluxifyed.Main;
 import flustix.fluxifyed.utils.ColorUtils;
 import flustix.fluxifyed.utils.FileUtils;
@@ -15,13 +12,27 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Arrays;
 
 public class ImageRenderer {
     public static boolean renderImage(RenderArgs args) {
         try {
-            JsonObject data = JsonParser.parseString(FileUtils.getResourceString("/images/" + args.template + ".json")).getAsJsonObject();
+            String data = FileUtils.getResourceString("/images/" + args.template + ".image");
+            String[] lines = data.split(";");
 
-            BufferedImage image = new BufferedImage(data.get("w").getAsInt(), data.get("h").getAsInt(), BufferedImage.TYPE_INT_ARGB);
+            int width = 0;
+            int height = 0;
+
+            String firstLine = lines[0];
+            if (firstLine.startsWith("size")) {
+                int index = firstLine.indexOf("(");
+                int index2 = firstLine.indexOf(")");
+                String[] size = firstLine.substring(index + 1, index2).split(",");
+                width = Integer.parseInt(size[0].strip());
+                height = Integer.parseInt(size[1].strip());
+            }
+
+            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             Graphics2D graphics = image.createGraphics();
             InputStream stream = Main.class.getResourceAsStream("/fonts/Quicksand-SemiBold.ttf"); //TODO: make font configurable
 
@@ -34,8 +45,9 @@ public class ImageRenderer {
             graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             graphics.setFont(font.deriveFont(24f));
 
-            for (JsonElement d : data.getAsJsonArray("d"))
-                renderObject(d.getAsJsonObject(), args.data, graphics);
+            for (String line : Arrays.stream(lines).skip(1).toArray(String[]::new)) {
+                if (!line.isBlank()) renderObject(line.strip(), args.data, graphics);
+            }
 
             graphics.dispose();
             ImageIO.write(image, "png", new File(args.output));
@@ -46,88 +58,111 @@ public class ImageRenderer {
         }
     }
 
-    static void renderObject(JsonObject json, RenderData data, Graphics2D g2d) {
-        switch (json.get("t").getAsString()) {
-            case "rect" -> renderRect(json.getAsJsonObject("d"), data, g2d);
-            case "xpProgress" -> renderXpProgress(json.getAsJsonObject("d"), data, g2d);
-            case "image" -> renderImage(json.getAsJsonObject("d"), data, g2d);
-            case "text" -> renderText(json.getAsJsonObject("d"), data, g2d);
+    private static void renderObject(String line, RenderData data, Graphics2D g2d) {
+        int index = line.indexOf("(");
+        int index2 = line.indexOf(")");
+
+        String[] args = line.substring(index + 1, index2).split(",");
+        String type = line.substring(0, index);
+
+        for (int i = 0; i < args.length; i++)
+            args[i] = args[i].strip();
+
+        switch (type) {
+            case "rect" -> renderRect(args, data, g2d);
+            case "xpbar" -> renderXpProgress(args, data, g2d);
+            case "image" -> renderImage(args, data, g2d);
+            case "text" -> renderText(args, data, g2d);
         }
     }
 
-    static void renderRect(JsonObject json, RenderData data, Graphics2D g2d) {
-        String f = json.get("f") == null ? "#ffffff" : json.get("f").getAsString(); // fill
-        int x = json.get("x") == null ? 0 : json.get("x").getAsInt(); // x
-        int y = json.get("y") == null ? 0 : json.get("y").getAsInt(); // y
-        int w = json.get("w") == null ? 0 : json.get("w").getAsInt(); // width
-        int h = json.get("h") == null ? 0 : json.get("h").getAsInt(); // height
-        int r = json.get("r") == null ? 0 : json.get("r").getAsInt(); // radius
+    /**
+     * rect(color, x, y, width, height, radius)
+     */
+    private static void renderRect(String[] args, RenderData data, Graphics2D g2d) {
+        String fill = args.length > 0 ? args[0] : "#ffffff";
+        int x = args.length > 1 ? tryParseInt(args[1], 0) : 0;
+        int y = args.length > 2 ? tryParseInt(args[2], 0) : 0;
+        int w = args.length > 3 ? tryParseInt(args[3], 0) : 0;
+        int h = args.length > 4 ? tryParseInt(args[4], 0) : 0;
+        int r = args.length > 5 ? tryParseInt(args[5], 0) : 0;
 
-        f = replaceVars(f, data);
+        Main.LOGGER.info("Rendering rect: " + fill + ", " + x + ", " + y + ", " + w + ", " + h + ", " + r);
 
-        g2d.setColor(ColorUtils.hexToRGBA(f));
+        fill = replaceVars(fill, data);
+
+        g2d.setColor(ColorUtils.hexToRGBA(fill));
         g2d.fillRoundRect(x, y, w, h, r, r);
     }
 
-    static void renderXpProgress(JsonObject json, RenderData data, Graphics2D g2d) {
-        String f = json.get("f") == null ? "#ffffff" : json.get("f").getAsString(); // fill
-        int x = json.get("x") == null ? 0 : json.get("x").getAsInt(); // x
-        int y = json.get("y") == null ? 0 : json.get("y").getAsInt(); // y
-        int w = json.get("w") == null ? 0 : json.get("w").getAsInt(); // max width
-        int h = json.get("h") == null ? 0 : json.get("h").getAsInt(); // height
-        int r = json.get("r") == null ? 0 : json.get("r").getAsInt(); // radius
+    /**
+     * xpbar(color, x, y, width, height, radius)
+     */
+    private static void renderXpProgress(String[] args, RenderData data, Graphics2D g2d) {
+        String fill = args.length > 0 ? args[0] : "#ffffff";
+        int x = args.length > 1 ? tryParseInt(args[1], 0) : 0;
+        int y = args.length > 2 ? tryParseInt(args[2], 0) : 0;
+        int width = args.length > 3 ? tryParseInt(args[3], 0) : 0;
+        int height = args.length > 4 ? tryParseInt(args[4], 0) : 0;
+        int radius = args.length > 5 ? tryParseInt(args[5], 0) : 0;
 
-        f = replaceVars(f, data);
+        fill = replaceVars(fill, data);
 
         float progress = Float.parseFloat(replaceVars("{xp.progress}", data));
-        w = (int) (w * (progress / 100f));
+        width = (int) (width * (progress / 100f));
 
-        g2d.setColor(ColorUtils.hexToRGBA(f));
-        g2d.fillRoundRect(x, y, w, h, r, r);
+        g2d.setColor(ColorUtils.hexToRGBA(fill));
+        g2d.fillRoundRect(x, y, width, height, radius, radius);
     }
 
-    static void renderImage(JsonObject json, RenderData data, Graphics2D g2d) {
-        String i = json.get("i") == null ? "url" : json.get("i").getAsString(); // image
-        int x = json.get("x") == null ? 0 : json.get("x").getAsInt(); // x
-        int y = json.get("y") == null ? 0 : json.get("y").getAsInt(); // y
-        int w = json.get("w") == null ? 0 : json.get("w").getAsInt(); // width
-        int h = json.get("h") == null ? 0 : json.get("h").getAsInt(); // height
-        int r = json.get("r") == null ? 0 : json.get("r").getAsInt(); // radius
+    /**
+     * image(url, x, y, width, height, radius)
+     */
+    private static void renderImage(String[] args, RenderData data, Graphics2D g2d) {
+        String url = args.length > 0 ? args[0] : "";
+        int x = args.length > 1 ? tryParseInt(args[1], 0) : 0;
+        int y = args.length > 2 ? tryParseInt(args[2], 0) : 0;
+        int width = args.length > 3 ? tryParseInt(args[3], 0) : 0;
+        int height = args.length > 4 ? tryParseInt(args[4], 0) : 0;
+        int radius = args.length > 5 ? tryParseInt(args[5], 0) : 0;
 
-        i = replaceVars(i, data);
+        url = replaceVars(url, data);
 
         try {
-            BufferedImage image = ImageIO.read(new URL(i));
-            BufferedImage scaledImage = GraphicsUtils.scaleImage(image, w, h);
-            BufferedImage roundedImage = GraphicsUtils.drawRoundedRect(scaledImage, r);
-            g2d.drawImage(roundedImage, x, y, w, h, null);
+            BufferedImage image = ImageIO.read(new URL(url));
+            BufferedImage scaledImage = GraphicsUtils.scaleImage(image, width, height);
+            BufferedImage roundedImage = GraphicsUtils.drawRoundedRect(scaledImage, radius);
+            g2d.drawImage(roundedImage, x, y, width, height, null);
         } catch (Exception ignored) {
         }
     }
 
-    static void renderText(JsonObject json, RenderData data, Graphics2D g2d) {
-        String t = json.get("t") == null ? "" : json.get("t").getAsString(); // text
-        String a = json.get("a") == null ? "l" : json.get("a").getAsString(); // alignment
-        String c = json.get("c") == null ? "#ffffff" : json.get("c").getAsString(); // color
-        int x = json.get("x") == null ? 0 : json.get("x").getAsInt(); // x
-        int y = json.get("y") == null ? 0 : json.get("y").getAsInt(); // y
-        int w = json.get("w") == null ? 0 : json.get("w").getAsInt(); // max width
-        int s = json.get("s") == null ? 0 : json.get("s").getAsInt(); // font size
+    /**
+     * text(text, x, y, color, size, align, width)
+     */
+    private static void renderText(String[] args, RenderData data, Graphics2D g2d) {
+        String text = args.length > 0 ? args[0] : "";
+        int x = args.length > 1 ? tryParseInt(args[1], 0) : 0;
+        int y = args.length > 2 ? tryParseInt(args[2], 0) : 0;
+        String color = args.length > 3 ? args[3] : "#ffffff";
+        int size = args.length > 4 ? tryParseInt(args[4], 24) : 24;
+        String align = args.length > 5 ? args[5] : "left";
+        int width = args.length > 6 ? tryParseInt(args[6], 1000) : 1000;
 
-        t = replaceVars(t, data);
-        c = replaceVars(c, data);
+        text = replaceVars(text, data);
+        color = replaceVars(color, data);
 
         Alignment alignment = Alignment.LEFT;
-        switch (a) {
-            case "c" -> alignment = Alignment.CENTER;
-            case "r" -> alignment = Alignment.RIGHT;
+        switch (align) {
+            case "center" -> alignment = Alignment.CENTER;
+            case "right" -> alignment = Alignment.RIGHT;
         }
 
-        g2d.setColor(ColorUtils.hexToRGBA(c));
-        GraphicsUtils.drawString(g2d, t, x, y, s, w, alignment);
+        g2d.setColor(ColorUtils.hexToRGBA(color));
+        GraphicsUtils.drawString(g2d, text, x, y, size, width, alignment);
     }
 
-    static String replaceVars(String s, RenderData data) {
+    private static String replaceVars(String s, RenderData data) {
         while (!data.loaded) {
             try {
                 Thread.sleep(100); // wait for data to load
@@ -143,5 +178,13 @@ public class ImageRenderer {
         }
 
         return s;
+    }
+
+    private static int tryParseInt(String s, int def) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return def;
+        }
     }
 }
