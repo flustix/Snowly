8774a104d8d93613b7e8ef0d15ba4735a9828ba6
Flux <flustix@foxes4life.net>
2024-01-18 02:39:56 +0100

    Missed a file

--- Snowly/Modules/XP/XpModule.cs
+++ Snowly/Modules/XP/XpModule.cs
@@ -13,11 +13,13 @@ using Snowly.Utils;
 
 namespace Snowly.Modules.XP;
 
-public class XpModule : IModule {
+public class XpModule : IModule
+{
     public string Name => "XP";
     public string Description => "Reward your members for chatting and being active.";
 
-    public List<ISlashCommand> SlashCommands => new() {
+    public List<ISlashCommand> SlashCommands => new()
+    {
         new RankCommand(),
         new TopCommand(),
         new XpCommand(),
@@ -32,13 +34,15 @@ public class XpModule : IModule {
         await Task.CompletedTask;
     }
 
-    public async Task OnMessageReceived(MessageCreateEventArgs args) {
+    public async Task OnMessageReceived(MessageCreateEventArgs args)
+    {
         if (args.Guild is null) return;
         if (args.Channel.IsPrivate) return;
         if (args.Author is not DiscordMember member) return;
         if (args.Author.IsBot) return;
+
         var guildConfig = Configs.GetGuildConfig(args.Guild.Id);
-        var xpEnabled = guildConfig?.XPEnabled ?? true;
+        var xpEnabled = guildConfig.XPEnabled;
 
         if (!xpEnabled) return;
 
@@ -51,11 +55,12 @@ public class XpModule : IModule {
         var mulitplier = 1d + mulitplierRoles.Sum(role => role.Multiplier);
         var channelMultiplier = XpUtils.GetMultiplierChannels(args.Guild.Id).FirstOrDefault(c => c.ChannelId == args.Channel.Id);
 
-        if (channelMultiplier is not null) {
+        if (channelMultiplier is not null)
+        {
             mulitplier *= channelMultiplier.Multiplier;
         }
 
-        var toAdd = (int) (new Random().Next(10, 20) * mulitplier);
+        var toAdd = (int)(new Random().Next(10, 20) * mulitplier);
         Snowly.Logger.LogDebug($"Adding {toAdd} XP to {args.Author.GetNickname()} ({args.Author.Id})");
         user.Xp += toAdd;
         user.LastMessage = DateTimeOffset.Now.ToUnixTimeSeconds();
@@ -64,21 +69,24 @@ public class XpModule : IModule {
 
         var userConfig = Configs.GetUserConfig(args.Author.Id);
         var levelUpMessages = userConfig?.LevelUpMessages ?? true;
-        levelUpMessages = levelUpMessages && (guildConfig?.LevelUpMessages ?? true);
+        levelUpMessages = levelUpMessages && (guildConfig.LevelUpMessages);
 
         var messageChannel = args.Channel;
 
         var levelUpChannel = args.Guild.Channels.FirstOrDefault(x => x.Key == guildConfig.LevelUpChannelID);
 
-        if (levelUpChannel.Value != null && levelUpChannel.Value.CanMessage()) {
+        if (levelUpChannel.Value != null && levelUpChannel.Value.CanMessage())
+        {
             messageChannel = levelUpChannel.Value;
             levelUpMessages = true;
         }
 
-        if (level != user.Level && levelUpMessages) {
+        if (level != user.Level && levelUpMessages)
+        {
             await messageChannel.SendMessageAsync(new CustomEmbed
                 {
-                    Author = new CustomEmbedAuthor {
+                    Author = new CustomEmbedAuthor
+                    {
                         Name = $"{args.Author.GetNickname()}",
                         IconUrl = args.Author.GetAvatarUrl(ImageFormat.Auto)
                     },
@@ -93,13 +101,15 @@ public class XpModule : IModule {
         await Task.CompletedTask;
     }
 
-    private static void handleRoles(XpUser user, DiscordMember member, DiscordGuild guild) {
+    private static void handleRoles(XpUser user, DiscordMember member, DiscordGuild guild)
+    {
         var roles = XpUtils.GetRewardRoles(guild.Id).OrderBy(x => x.Level).ToList();
         if (!roles.Any()) return;
 
         var rolesToAdd = roles.Where(x => x.Level <= user.Level);
 
-        foreach (var role in rolesToAdd) {
+        foreach (var role in rolesToAdd)
+        {
             var role1 = guild.Roles.FirstOrDefault(x => x.Value.Id == role.RoleId).Value;
             if (role1 is null) continue;
 
@@ -110,7 +120,8 @@ public class XpModule : IModule {
 
         var rolesToRemove = roles.Where(x => x.Level > user.Level);
 
-        foreach (var role in rolesToRemove) {
+        foreach (var role in rolesToRemove)
+        {
             var role1 = guild.Roles.FirstOrDefault(x => x.Value.Id == role.RoleId).Value;
             if (role1 is null) continue;
 
